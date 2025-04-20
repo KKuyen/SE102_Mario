d939ff377e7ea508fb32a9d2adc9d56cebc8cc2c
NDHunq <22520514@gm.uit.edu.vn>
2025-04-20 11:21:31 +0700

    render temporary content on the transcript

--- 05-SceneManager/Transcript.cpp
+++ 05-SceneManager/Transcript.cpp
@@ -1,25 +1,107 @@
 #include "Transcript.h"
 #include "Game.h"
+#include "PlayScene.h"
 
 void CTranscript::Render()
 {
- 	float screenWidth = (float)CGame::GetInstance()->GetBackBufferWidth();
-	float screenHeight = (float)CGame::GetInstance()->GetBackBufferHeight();
- 	float camX, camY;
-	CGame::GetInstance()->GetCamPos(camX, camY);
-  	float renderX = x + camX;
-	float renderY = y + camY;
+    float screenWidth = (float)CGame::GetInstance()->GetBackBufferWidth();
+    float screenHeight = (float)CGame::GetInstance()->GetBackBufferHeight();
+    float camX, camY;
+    CGame::GetInstance()->GetCamPos(camX, camY);
+    float renderX = x + camX;
+    float renderY = y + camY;
 
-	CSprites* s = CSprites::GetInstance();
-	s->Get(SPRITE_ID)->Draw(renderX, renderY);
-
-	//RenderBoundingBox();
+    CSprites* s = CSprites::GetInstance();
+    LPSPRITE sprite = s->Get(SPRITE_ID);
+    if (sprite != nullptr)
+    {
+        sprite->Draw(renderX, renderY);
+    }
+    else
+    {
+        DebugOut(L"[ERROR] Transcript sprite ID %d not found\n", SPRITE_ID);
+    }
+	//Draw score
+     for (int i = 0; i < 7; i++)
+    {
+        LPSPRITE digitSprite = s->Get(NUMBER_0 + scoreDigits[i]);
+        if (digitSprite != nullptr)
+        {
+            digitSprite->Draw(renderX -63 + i * 8, renderY -3);
+        }
+        else
+        {
+            DebugOut(L"[ERROR] Number sprite ID %d not found\n", NUMBER_0 + scoreDigits[i]);
+        }
+    }
+	 //Draw time
+     for (int i = 0; i < 3; i++)
+    {
+        int digitIndex = 2 - i; 
+        LPSPRITE digitSprite = s->Get(NUMBER_0 + timeDigits[digitIndex]);
+        if (digitSprite != nullptr)
+        {
+            digitSprite->Draw(renderX +8 + i * 8, renderY -3); 
+        }
+        else
+        {
+            DebugOut(L"[ERROR] Number sprite ID %d not found\n", NUMBER_0 + timeDigits[digitIndex]);
+        }
+    }
+	 //Draw life
+     LPSPRITE digitSprite = s->Get(NUMBER_0 + life);
+     if (digitSprite != nullptr)
+     {
+         digitSprite->Draw(renderX  -85, renderY - 3);
+     }
+     else
+     {
+         DebugOut(L"[ERROR] Number sprite ID %d not found\n", NUMBER_0 +life);
+     }
+	 //Draw dollar
+	 for (int i = 0; i < 2; i++)
+	 {
+		 int digitIndex = 2 - i;
+		 LPSPRITE digitSprite3 = s->Get(NUMBER_0 + dollarDigits[digitIndex]);
+		 if (digitSprite3 != nullptr)
+		 {
+             digitSprite3->Draw(renderX +18 + i * 8, renderY - 11);
+		 }
+		 else
+		 {
+			 DebugOut(L"[ERROR] Number sprite ID %d not found\n", NUMBER_0 + dollarDigits[digitIndex]);
+		 }
+	 }
+	 //Draw flag
+     for (int i = 0; i < flag; i++)
+     {
+		 LPSPRITE digitSprite = s->Get(NUMBER_0 + 10);
+		 if (digitSprite != nullptr)
+		 {
+			 digitSprite->Draw(renderX - 63 + i * 8, renderY - 11);
+		 }
+		 else
+		 {
+			 DebugOut(L"[ERROR] Number sprite ID %d not found\n", NUMBER_0 + 10);
+		 }
+     }
+     LPSPRITE digitSprite4 = s->Get(NUMBER_0 + 11);
+     if (digitSprite4 != nullptr)
+     {
+         digitSprite4->Draw(renderX - 10, renderY - 11);
+     }
+     else
+     {
+         DebugOut(L"[ERROR] Number sprite ID %d not found\n", NUMBER_0 + life);
+     }
+       
+    //RenderBoundingBox();
 }
 
 void CTranscript::GetBoundingBox(float& l, float& t, float& r, float& b)
 {
-	l = x -  TRANSCRIPT_WIDTH / 2 + 1;
-	t = y -  TRANSCRIPT_HEIGHT / 2;
-	r = l +  TRANSCRIPT_WIDTH - 1;
-	b = t +  TRANSCRIPT_HEIGHT;
+    l = x - TRANSCRIPT_WIDTH / 2 + 1;
+    t = y - TRANSCRIPT_HEIGHT / 2;
+    r = l + TRANSCRIPT_WIDTH - 1;
+    b = t + TRANSCRIPT_HEIGHT;
 }
--- 05-SceneManager/Transcript.h
+++ 05-SceneManager/Transcript.h
@@ -7,12 +7,49 @@
 #define TRANSCRIPT_WIDTH 263
 #define TRANSCRIPT_HEIGHT 74
 
+#define NUMBER_0 230000
+#define NUMBER_1 230001
+#define NUMBER_2 230002
+#define NUMBER_3 230003
+#define NUMBER_4 230004
+#define NUMBER_5 230005
+#define NUMBER_6 230006
+#define NUMBER_7 230007
+#define NUMBER_8 230008
+#define NUMBER_9 230009
+
 class CTranscript : public CGameObject {
 protected:
+	int score;
+	int scoreDigits[7];
+	int time;
+	int timeDigits[3];
+	int life;
+	int dollar;
+	int dollarDigits[2];
+	int flag;
+
 
 public:
 	CTranscript(float x, float y) : CGameObject(x, y)
 	{
+		score = 3000;
+		time = 123;
+		life = 3;
+		dollar = 87;
+		flag = 6;
+		scoreDigits[0] = score % 10;
+		scoreDigits[1] = (score / 10) % 10;
+		scoreDigits[2] = (score / 100) % 10;
+		scoreDigits[3] = (score / 1000) % 10;
+		scoreDigits[4] = (score / 10000) % 10;
+		scoreDigits[5] = (score / 100000) % 10;
+		scoreDigits[6] = (score / 1000000) % 10;
+		timeDigits[0] = time % 10;
+		timeDigits[1] = (time / 10) % 10;
+		timeDigits[2] = (time / 100) % 10;
+		dollarDigits[0] = dollar % 10;
+		dollarDigits[1] = (dollar / 10) % 10;
 
 	}
 	void Render();
